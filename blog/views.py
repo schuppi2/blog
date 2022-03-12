627bb3998ae0a08cdf85f617fea81345c96a1e64
schuppi2 <neunmaleins@gmail.com>
2022-03-12 15:59:40 +0100

    added post_detail.html and reg in urls.py

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+
+#created for detail views of post, url in urls.py /post/1/ (i.e.)
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 # Create your views here.
 def post_list(request):
